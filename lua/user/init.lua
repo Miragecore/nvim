64417f41477387cb65bb4088ec599d1a1d788af5
yskim <miragekimys@gmail.com>
2022-11-21 01:47:18 +0900

    init

---
+++ lua/user/init.lua
@@ -0,0 +1,10 @@
+print("user init")
+
+require ("user.set")
+
+require ("user.packer")
+require ("user.tokyonight")
+require ("user.telescope")
+
+require ("user.lsp")
+
---
+++ lua/user/packer.lua
@@ -0,0 +1,32 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  use 'folke/tokyonight.nvim'
+
+  use 'nvim-lua/plenary.nvim'
+
+  use 'nvim-telescope/telescope-fzf-native.nvim'
+
+  use {
+      'nvim-treesitter/nvim-treesitter',
+      run = function()
+          local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+          ts_update()
+      end,
+  }
+
+  use 'nvim-tree/nvim-web-devicons'
+  use 'nvim-telescope/telescope.nvim'
+
+  use {
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+      "neovim/nvim-lspconfig",
+  }
+  end)
